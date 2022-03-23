ce39ecdd1f1c1c797afa658fe6be6adb6f06f58c
liranbalilti <45334346+liranbalilti@users.noreply.github.com>
2022-03-23 10:32:26 +0200

    Update yelp.js
    init

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer 8E0OecPvZcqfzqMg9Ju9b5mM3A76lUPEeGu8-kNHvi_D5stqCdsIRvqmM7gJUw9U1korvytyscvcEMiWywsdwAZTGR3ho1Eo8TLA9uZR6NOX5A0lXehfCSE7PHWNX3Yx'
+        Authorization: 'API KEY'
     }
 });
